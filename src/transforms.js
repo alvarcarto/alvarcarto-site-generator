58c7c2c7ca3665fb19c696508b12c55c08254f5d
Kimmo Brunfeldt <kimmobrunfeldt@gmail.com>
2018-02-26 00:36:03 +0200

    Move html validation as final step

--- src/transforms.js
+++ src/transforms.js
@@ -1,8 +1,7 @@
 module.exports = [
-  { name: 'validate-html', transform: require('./transforms/validate-html'), delay: 10 },
-
   // Disabled for now
   // { name: 'prettify', transform: require('./transforms/prettify') },
 
   { name: 'base-url-change', transform: require('./transforms/base-url-change') },
+  { name: 'validate-html', transform: require('./transforms/validate-html'), delay: 10 },
 ];
