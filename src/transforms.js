b66c5708af9ade67be1d3e0ab92d0730f6722ddd
Kimmo Brunfeldt <kimmobrunfeldt@gmail.com>
2018-02-25 16:44:40 +0200

    Disable prettify transform

--- src/transforms.js
+++ src/transforms.js
@@ -1,4 +1,6 @@
 module.exports = [
   { name: 'validate-html', transform: require('./transforms/validate-html'), delay: 10 },
-  { name: 'prettify', transform: require('./transforms/prettify') },
+
+  // Disabled for now
+  // { name: 'prettify', transform: require('./transforms/prettify') },
 ];
